27342f450fe4e54e6311086c15effe6916f526c2
bbx8216 <bbx8216@gmail.com>
2023-03-31 16:54:24 +0900

    Feat: 텍스트 페이지 프레임만 구현

--- lib/AddFile/addTextPage.dart
+++ lib/AddFile/addTextPage.dart
@@ -6,7 +6,25 @@ class AddTextPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: const Color(0xffFED40B),
+      appBar: AppBar(
+      elevation: 0.0,
+      backgroundColor: Color(0xffFED40B),
+      title: Text(
+        '텍스트 붙여넣어 학습자료 만들기',
+        style: TextStyle(
+            color: Colors.black, fontFamily: 'Dongle', fontSize: 35),
+      ),
+      centerTitle: true,
+      leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_new),
+          color: const Color(0xff5a4c0c),
+          onPressed: () {
+            Navigator.pop(context);
+          }),
+    ),
     );
   }
+
+
 }
