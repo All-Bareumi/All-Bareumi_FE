42343e8b2094de99dee0adaa6e85b6e3ee91466c
bbx8216 <bbx8216@gmail.com>
2023-05-11 16:17:36 +0900

    Fix: userdrawer - late initialize 에러 해결

--- lib/loadingDrawer.dart
+++ lib/loadingDrawer.dart
@@ -19,50 +19,79 @@ class _LoadingDrawerState extends State<LoadingDrawer> {
   late String userName;
   late String profileImg;
 
-  void getUserData() async {
+  Future<User> getUserData() async {
     //카카오에서 받아오는 user 정보
-    try {
-      user = await UserApi.instance.me();
-      userNameNP = user.kakaoAccount?.profile?.nickname;
-      profileImgNP = user.kakaoAccount?.profile?.profileImageUrl;
-      userId = user.id;
-      notNullUser = true;
-    } catch (error) {
-      notNullUser = false;
-    }
-    print(user);
-    userName = userNameNP ?? "사용자";
-    profileImg = profileImgNP ?? "image/logo/logo.png";
+    user = await UserApi.instance.me();
+    return user;
   }
 
   @override
   void initState() {
     super.initState();
-    getUserData();
+    //getUserData();
   }
+
   @override
   Widget build(BuildContext context) {
-    return notNullUser
-        ? myDrawer(
-            user: user,
-            userName: userName,
-            userId: userId,
-            profileImg: profileImg)
-        : Drawer(
-            child: Padding(
-            padding: const EdgeInsets.fromLTRB(20, 70, 0, 0),
-            child: Center(
-              child: Column(children: <Widget>[
-                Text(
-                  '사용자 정보를',
-                  style: TextStyle(fontSize: 40, fontFamily: 'Dongle'),
-                ),
-                Text('불러오지 못했습니다.',
-                    style: TextStyle(fontSize: 40, fontFamily: 'Dongle')),
-              ]),
-            ),
-          )
-    );
-    //print(notNullUser);
+    return FutureBuilder(
+        future: getUserData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          userNameNP = user.kakaoAccount?.profile?.nickname;
+          profileImgNP = user.kakaoAccount?.profile?.profileImageUrl;
+          userId = user.id;
+          userName = userNameNP ?? "사용자";
+          profileImg = profileImgNP ?? "image/logo/logo.png";
+          if (snapshot.hasData == false) {
+            return Drawer(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Drawer(
+                child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 70, 0, 0),
+              child: Center(
+                child: Column(children: <Widget>[
+                  Text(
+                    '사용자 정보를',
+                    style: TextStyle(fontSize: 40, fontFamily: 'Dongle'),
+                  ),
+                  Text('불러오지 못했습니다.',
+                      style: TextStyle(fontSize: 40, fontFamily: 'Dongle')),
+                ]),
+              ),
+            ));
+          } else {
+            return myDrawer(
+                user: user,
+                userName: userName,
+                userId: userId,
+                profileImg: profileImg);
+          }
+        });
   }
+
+// Widget build(BuildContext context) {
+//   return notNullUser
+//       ? myDrawer(
+//           user: user,
+//           userName: userName,
+//           userId: userId,
+//           profileImg: profileImg)
+//       : Drawer(
+//           child: Padding(
+//           padding: const EdgeInsets.fromLTRB(20, 70, 0, 0),
+//           child: Center(
+//             child: Column(children: <Widget>[
+//               Text(
+//                 '사용자 정보를',
+//                 style: TextStyle(fontSize: 40, fontFamily: 'Dongle'),
+//               ),
+//               Text('불러오지 못했습니다.',
+//                   style: TextStyle(fontSize: 40, fontFamily: 'Dongle')),
+//             ]),
+//           ),
+//         )
+//   );
+//   //print(notNullUser);
+// }
 }
