82789476475b169e1e64077e7f91d7c9bc8c3522
bbx8216 <bbx8216@gmail.com>
2023-05-25 12:18:31 +0900

    Test: imagePicker.dart 파일 주석 처리

---
+++ lib/SetCharacter/imagePicker.dart
@@ -0,0 +1,29 @@
+// import 'package:flutter/material.dart';
+// import 'package:hexcolor/hexcolor.dart';
+//
+// class ImagePicker extends StatelessWidget {
+//   const ImagePicker({Key? key}) : super(key: key);
+//   final String hexYellow = "FED40B";
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         child: Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: <Widget>[
+//         Icon(
+//           Icons.add_photo_alternate,
+//           color: HexColor(hexYellow),
+//           size: 150,
+//         ),
+//         Padding(
+//           padding: EdgeInsets.only(top: 20),
+//           child: ElevatedButton(
+//             onPressed: () => takeImage(context),
+//             child: Text('사진 올리기'),
+//           ),
+//         )
+//       ],
+//     ));
+//   }
+// }
