a52eebd90e7626696fc531bd32fe8560118926f5
bbx8216 <bbx8216@gmail.com>
2023-05-26 16:26:30 +0900

    Feat: 이미지 서버로 업로드 성공

--- lib/SetCharacter/setMyAvatar.dart
+++ lib/SetCharacter/setMyAvatar.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:camera/camera.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:image_picker/image_picker.dart';
@@ -8,32 +9,60 @@ import 'package:image_picker/image_picker.dart';
 import '../userDrawer/loadingDrawer.dart';
 
 class SetMyAvatar extends StatefulWidget {
-  const SetMyAvatar({Key? key}) : super(key: key);
-
+  const SetMyAvatar({Key? key, required this.login_token}) : super(key: key);
+  final String login_token;
   @override
   State<SetMyAvatar> createState() => _SetMyAvatarState();
 }
 
 class _SetMyAvatarState extends State<SetMyAvatar> {
+  final String hexYellow = "FED40B";
+  final String hexGreen = "1AB846";
+  final String hexBlue = "1086fe";
+  final String hexRed = "ED5555";
 
-  final String hexYellow ="FED40B";
-  final String hexGreen ="1AB846";
-  final String hexBlue ="1086fe";
-  final String hexRed ="ED5555";
 
   XFile? _image; //이미지를 담을 변수 선언
+  dynamic _sendImage; // 이미지 서버로 보낼 변수 선언
   final ImagePicker picker = ImagePicker(); //ImagePicker 초기화
-
   //이미지를 가져오는 함수
-  Future getImage(ImageSource imageSource) async {
+  Future<dynamic> getImage(ImageSource imageSource) async {
     //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
     final XFile? pickedFile = await picker.pickImage(source: imageSource);
     if (pickedFile != null) {
+      _sendImage = pickedFile.path;
       setState(() {
-        _image = XFile(pickedFile.path); //가져온 이미지를 _image에 저장
+        _image = XFile(_sendImage); //가져온 이미지를 _image에 저장
       });
+      print('이미지 화면에 띄우기');
+
+      // 서버에 사진 업로드
+      print('사진을 서버에 업로드 합니다.');
+      var dio = new Dio();
+      print(_sendImage);
+      FormData formDataImg = FormData.fromMap(
+          {'data': await MultipartFile.fromFile(_sendImage)}); // 여기서 image는 서버의 key값
+
+      try {
+        dio.options.contentType = 'multipart/form-data';
+        dio.options.maxRedirects.isFinite;
+        dio.options.headers = {
+          HttpHeaders.authorizationHeader: 'Bearer ${widget.login_token}' // 이 형식으로 항상 넘겨주기
+        }; // login token
+        var response = await dio.post(
+          'http://localhost:8001/api/user/photos/upload',
+          data: formDataImg,
+        );
+        print('성공적으로 업로드했습니다');
+        print(response.data);
+        return response.data;
+      } catch (e) {
+        print(e);
+      }
     }
   }
+
+
 
   Widget _buildPhotoArea() {
     return _image != null
@@ -56,17 +85,23 @@ class _SetMyAvatarState extends State<SetMyAvatar> {
       children: [
         ElevatedButton(
           onPressed: () {
-            getImage(ImageSource.camera); //getImage 함수를 호출해서 카메라로 찍은 사진 가져오기
+            var image_response = getImage(ImageSource.camera); //getImage 함수를 호출해서 카메라로 찍은 사진 가져오기
           },
-          child: Text("카메라", style: TextStyle(fontFamily: 'Dongle', fontSize: 30),),
+          child: Text(
+            "카메라",
+            style: TextStyle(fontFamily: 'Dongle', fontSize: 30),
+          ),
           style: ElevatedButton.styleFrom(backgroundColor: HexColor(hexBlue)),
         ),
         SizedBox(width: 30),
         ElevatedButton(
           onPressed: () {
-            getImage(ImageSource.gallery); //getImage 함수를 호출해서 갤러리에서 사진 가져오기
+            var image_response = getImage(ImageSource.gallery); //getImage 함수를 호출해서 갤러리에서 사진 가져오기
           },
-          child: Text("갤러리",  style: TextStyle(fontFamily: 'Dongle', fontSize: 30),),
+          child: Text(
+            "갤러리",
+            style: TextStyle(fontFamily: 'Dongle', fontSize: 30),
+          ),
           style: ElevatedButton.styleFrom(backgroundColor: HexColor(hexGreen)),
         ),
       ],
@@ -82,14 +117,12 @@ class _SetMyAvatarState extends State<SetMyAvatar> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.start,
-
         children: [
           SizedBox(height: 100, width: double.infinity),
           _buildPhotoArea(),
           SizedBox(height: 50),
           _buildButton(),
           SizedBox(height: 50),
-
         ],
       ),
     );
@@ -102,7 +135,7 @@ class _SetMyAvatarState extends State<SetMyAvatar> {
       title: Text(
         '내 아바타 설정',
         style:
-        TextStyle(color: Colors.black, fontFamily: 'Dongle', fontSize: 35),
+            TextStyle(color: Colors.black, fontFamily: 'Dongle', fontSize: 35),
       ),
       leading: IconButton(
           icon: Icon(Icons.arrow_back_ios_new),
