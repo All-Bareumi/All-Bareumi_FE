36f1894783454fd1faa882e187dd6dd68241408e
bbx8216 <bbx8216@gmail.com>
2023-05-28 17:48:29 +0900

    Feat: 이미지 업로드 시 확인받기

--- lib/imageUploader.dart
+++ lib/imageUploader.dart
@@ -6,63 +6,87 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:image_picker/image_picker.dart';
 
-import '../userDrawer/loadingDrawer.dart';
+import 'userDrawer/loadingDrawer.dart';
 
-class SetMyAvatar extends StatefulWidget {
-  const SetMyAvatar({Key? key, required this.login_token}) : super(key: key);
+class ImageUploader extends StatefulWidget {
+  const ImageUploader(
+      {Key? key, required this.login_token, required this.pageName})
+      : super(key: key);
   final String login_token;
+  final String pageName;
+
   @override
-  State<SetMyAvatar> createState() => _SetMyAvatarState();
+  State<ImageUploader> createState() => _ImageUploaderState();
 }
 
-class _SetMyAvatarState extends State<SetMyAvatar> {
+class _ImageUploaderState extends State<ImageUploader> {
   final String hexYellow = "FED40B";
   final String hexGreen = "1AB846";
   final String hexBlue = "1086fe";
   final String hexRed = "ED5555";
 
-
   XFile? _image; //이미지를 담을 변수 선언
   dynamic _sendImage; // 이미지 서버로 보낼 변수 선언
   final ImagePicker picker = ImagePicker(); //ImagePicker 초기화
   //이미지를 가져오는 함수
-  Future<dynamic> getImage(ImageSource imageSource) async {
+  Future getImage(ImageSource imageSource, BuildContext context) async {
     //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
     final XFile? pickedFile = await picker.pickImage(source: imageSource);
     if (pickedFile != null) {
+      //팝업 페이지 띄우기
       _sendImage = pickedFile.path;
       setState(() {
         _image = XFile(_sendImage); //가져온 이미지를 _image에 저장
       });
       print('이미지 화면에 띄우기');
-
-      // 서버에 사진 업로드
-      print('사진을 서버에 업로드 합니다.');
-      var dio = new Dio();
-      print(_sendImage);
-      FormData formDataImg = FormData.fromMap(
-          {'data': await MultipartFile.fromFile(_sendImage)}); // 여기서 image는 서버의 key값
-
-      try {
-        dio.options.contentType = 'multipart/form-data';
-        dio.options.maxRedirects.isFinite;
-        dio.options.headers = {
-          HttpHeaders.authorizationHeader: 'Bearer ${widget.login_token}' // 이 형식으로 항상 넘겨주기
-        }; // login token
-        var response = await dio.post(
-          'http://localhost:8001/api/user/photos/upload',
-          data: formDataImg,
-        );
-        print('성공적으로 업로드했습니다');
-        print(response.data);
-        return response.data;
-      } catch (e) {
-        print(e);
-      }
+    }
+    if (_image != null) {
+      _showDialog(context);
     }
   }
 
+  Future<dynamic> _showDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text('이 사진을 업로드할까요?'),
+        actions: [
+          ElevatedButton(
+              onPressed: () async {
+                Navigator.of(context).pop();
 
+                print('사진을 서버에 업로드 합니다.');
+                var dio = new Dio();
+                print(_sendImage);
+                FormData formDataImg = FormData.fromMap({
+                  'data': await MultipartFile.fromFile(_sendImage)
+                }); // 여기서 image는 서버의 key값
+                try {
+                  dio.options.contentType = 'multipart/form-data';
+                  dio.options.maxRedirects.isFinite;
+                  dio.options.headers = {
+                    HttpHeaders.authorizationHeader:
+                        'Bearer ${widget.login_token}' // 이 형식으로 항상 넘겨주기
+                  }; // login token
+                  var response = await dio.post(
+                    'http://localhost:8001/api/user/photos/upload',
+                    data: formDataImg,
+                  );
+                  print('성공적으로 업로드했습니다');
+                  print(response.data);
+                  return response.data;
+                } catch (e) {
+                  print(e);
+                }
+
+              },
+              child: Text('네')),
+          ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(), child: Text('아니요')),
+        ],
+      ),
+    );
+  }
 
   Widget _buildPhotoArea() {
     return _image != null
@@ -85,7 +109,11 @@ class _SetMyAvatarState extends State<SetMyAvatar> {
       children: [
         ElevatedButton(
           onPressed: () {
-            var image_response = getImage(ImageSource.camera); //getImage 함수를 호출해서 카메라로 찍은 사진 가져오기
+            getImage(ImageSource.camera,
+                context); //getImage 함수를 호출해서 카메라로 찍은 사진 가져오기
+            if (_image != null) {
+              //_showDialog(context);
+            }
           },
           child: Text(
             "카메라",
@@ -96,7 +124,11 @@ class _SetMyAvatarState extends State<SetMyAvatar> {
         SizedBox(width: 30),
         ElevatedButton(
           onPressed: () {
-            var image_response = getImage(ImageSource.gallery); //getImage 함수를 호출해서 갤러리에서 사진 가져오기
+            getImage(
+                ImageSource.gallery, context); //getImage 함수를 호출해서 갤러리에서 사진 가져오기
+            if (_image != null) {
+              //_showDialog(context);
+            }
           },
           child: Text(
             "갤러리",
@@ -133,7 +165,7 @@ class _SetMyAvatarState extends State<SetMyAvatar> {
       elevation: 0.0,
       backgroundColor: Color(0xffFED40B),
       title: Text(
-        '내 아바타 설정',
+        '${widget.pageName}',
         style:
             TextStyle(color: Colors.black, fontFamily: 'Dongle', fontSize: 35),
       ),
